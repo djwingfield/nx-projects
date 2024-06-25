a7b45de71a61870dd4b3eb7e5cc67786322603ad
Dom Wingfield <dom.j.wingfield@gmail.com>
2024-06-25 15:54:15 +0100

    Create and use sidebar

--- apps/next-banking/src/app/(root)/layout.tsx
+++ apps/next-banking/src/app/(root)/layout.tsx
@@ -1,7 +1,16 @@
+import Sidebar from '../../components/Sidebar';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>SIDEBAR{children}</main>;
+  const loggedIn = { firstName: 'Dom', lastName: 'Wingfield' };
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}></Sidebar>
+      {children}
+    </main>
+  );
 }
