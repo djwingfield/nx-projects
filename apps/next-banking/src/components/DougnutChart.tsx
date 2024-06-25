edd5e4a340e983670f9785cda96dba4ca29faa8a
Dom Wingfield <dom.j.wingfield@gmail.com>
2024-06-25 12:58:27 +0100

    Add balance box with charting

---
+++ apps/next-banking/src/components/DougnutChart.tsx
@@ -0,0 +1,20 @@
+import { ArcElement, Chart, ChartData } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+Chart.register(ArcElement);
+
+const DougnutChart = ({ accounts }: DoughnutChartProps) => {
+  const data: ChartData<'doughnut'> = {
+    datasets: [
+      {
+        label: 'Banks',
+        data: [1250, 2500, 3750],
+        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'],
+      },
+    ],
+    labels: ['Bank 1', 'Bank 2', 'Bank 3'],
+  };
+
+  return <Doughnut data={data} options={{ cutout: '60%' }} />;
+};
+
+export default DougnutChart;
